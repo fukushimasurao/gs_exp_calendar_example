46a162dca9e8558a6dfa67a63dba98486e07319f
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2025-02-16 18:57:43 +0900

    routeの確認

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'HELLO WORLD';
+});
+
+Route::get('/hello-html', function () {
+    return '<h1>HELLO WORLD</h1>';
+});
