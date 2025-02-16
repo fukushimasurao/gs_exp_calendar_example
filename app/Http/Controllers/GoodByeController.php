3c3c1a6fb5c0c7241f07669a1b91e05c1afce47e
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2025-02-16 20:28:15 +0900

    controller追加

---
+++ app/Http/Controllers/GoodByeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GoodByeController extends Controller
+{
+    public function goodBye()
+    {
+        return 'good bye';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\GoodByeController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'HELLO WORLD';
-});
+// ⭐️⭐️⭐️⭐️ ↓このrouteはコメントアウト or 消してください。
+//Route::get('/hello', function () {
+//    return 'HELLO WORLD';
+//});
 
 Route::get('/hello-html', function () {
     return '<h1>HELLO WORLD</h1>';
 });
+
+Route::get('/hello', [HelloController::class, 'hello']);
+Route::get('/good-bye', [GoodByeController::class, 'goodBye']);
