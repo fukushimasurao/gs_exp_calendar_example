12d8a297d265c72e9e59d24afd488aec22458574
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2025-02-22 05:01:47 +0900

    次回に向けて/宿題

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\HelloController;
-use App\Http\Controllers\GoodByeController;
-
-// ⭐️追加！
-use App\Http\Controllers\MemberController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
 
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello-html', function () {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+route::get('/hello-html', function () {
     return '<h1>HELLO WORLD</h1>';
 });
 
@@ -25,3 +30,10 @@ Route::get('/good-bye', [GoodByeController::class, 'goodBye']);
 Route::get('/members', [MemberController::class, 'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/products/create', [ProductController::class, 'create']); // フォーム表示
+Route::post('/products', [ProductController::class, 'store']); // データ登録
+
+require __DIR__.'/auth.php';
+
+
